fbb18f25faada86e3172b6f4b1f9a9a822f33870
ngolin <paindo@163.com>
2017-10-19 21:55:23 +0800

    add .toArray()

--- src/shared/liset/treemap.ts
+++ src/shared/liset/treemap.ts
@@ -5,7 +5,6 @@ class Item<Key, Val> {
   left: Item<Key, Val> = null;
   red = true;
   size = 1;
-
   constructor(public key: Key, public val: Val) { }
 
 }
@@ -16,10 +15,6 @@ class Item<Key, Val> {
  * 2. and making the key member readonly.
  * 
  * `val` isn't marked as readonly to be mutable and therefore to implement map functionality.
- * 
- * @interface Entry
- * @template Key 
- * @template Val 
  */
 interface Entry<Key, Val> {
   readonly key: Key;
@@ -44,12 +39,12 @@ export default class RedBlackBinarySearchMap<Key, Val> {
     // if (key === null) throw 'argument to get() is null';
     return this.GET(this.root, key);
   }
-  private GET(item: Item<Key, Val>, key: Key) {
-    while (item !== null) {
-      const cmp = this.comptor(key, item.key);
-      if (cmp < 0) item = item.left;
-      else if (cmp > 0) item = item.right;
-      else return item.val;
+  private GET(head: Item<Key, Val>, key: Key) {
+    while (head !== null) {
+      const cmp = this.comptor(key, head.key);
+      if (cmp < 0) head = head.left;
+      else if (cmp > 0) head = head.right;
+      else return head.val;
     }
     return null;
   }
@@ -58,7 +53,14 @@ export default class RedBlackBinarySearchMap<Key, Val> {
     return this.GET(this.root, key) !== null;
   }
 
-  put(key: Key, val: Val) {
+  /**
+   * 1. if the `key` has already existed in the map,
+   * it swaps the old value with the new one, 
+   * and finally return the old one.
+   * 2. otherwise it puts the new (`key`, `val`) pair 
+   * into the map and return null.
+   */
+  put(key: Key, val: Val): Val | null {
     // if (key === null) throw 'first argument to put() is null';
     // if (val === null) throw 'second argument to put() is null';
     let oldVal = null;
@@ -78,6 +80,7 @@ export default class RedBlackBinarySearchMap<Key, Val> {
     this.root = PUT(this.root, key, val);
     this.root.red = false;
     return oldVal;
+
   }
 
 
@@ -90,55 +93,51 @@ export default class RedBlackBinarySearchMap<Key, Val> {
       this.root.red = true;
     }
     let oldVal = null;
-    const DEL = (head: Item<Key, Val>, key: Key): Item<Key, Val> => {
-      if (this.comptor(key, head.key) < 0) {
-        if (!isRed(head.left) && !isRed(head.left.left)) {
-          head = moveRedLeft(head);
+    const DEL = (hole: Item<Key, Val>, key: Key): Item<Key, Val> => {
+      if (this.comptor(key, hole.key) < 0) {
+        if (!isRed(hole.left) && !isRed(hole.left.left)) {
+          hole = moveRedLeft(hole);
         }
-        head.left = DEL(head.left, key);
+        hole.left = DEL(hole.left, key);
       } else {
-        if (isRed(head.left)) head = rotateRight(head);
-        if (this.comptor(key, head.key) === 0 && (head.right === null)) return null;
-        if (!isRed(head.right) && !isRed(head.right.left)) head = moveRedRight(head);
-        if (this.comptor(key, head.key) === 0) {
-          const x = this.MIN(head.right);
-          head.key = x.key;
-          [oldVal, head.val] = [head.val, x.val];
-          head.right = deleteMin(head.right);
-        } else head.right = DEL(head.right, key);
+        if (isRed(hole.left)) hole = rotateRight(hole);
+        if (this.comptor(key, hole.key) === 0 && (hole.right === null)) return null;
+        if (!isRed(hole.right) && !isRed(hole.right.left)) hole = moveRedRight(hole);
+        if (this.comptor(key, hole.key) === 0) {
+          const shim = this.MIN(hole.right);
+          hole.key = shim.key;
+          [oldVal, hole.val] = [hole.val, shim.val];
+          hole.right = deleteMin(hole.right);
+        } else hole.right = DEL(hole.right, key);
       }
-      return balance(head);
+      return balance(hole);
     }
     this.root = DEL(this.root, key);
     if (this.root !== null) this.root.red = false;
     return oldVal;
+
   }
 
 
   min(): Entry<Key, Val> | null {
-    if (this.root === null) return null;
-    return this.MIN(this.root);
+    return this.root && this.MIN(this.root) || null;
   }
-  private MIN(root: Item<Key, Val>): Item<Key, Val> {
-    return root.left === null ? root : this.MIN(root.left);
+  private MIN(head: Item<Key, Val>): Item<Key, Val> {
+    return head.left && this.MIN(head.left) || head;
   }
   max(): Entry<Key, Val> | null {
-    if (this.root === null) return null;
-    return this.MAX(this.root);
+    return this.root && this.MAX(this.root) || null;
   }
-  private MAX(x: Item<Key, Val>) {
-    if (x.right === null) return x;
-    else return this.MAX(x.right);
+  private MAX(head: Item<Key, Val>) {
+    return head.right && this.MAX(head.right) || head;
   }
   of(index: number): Entry<Key, Val> | null {
-    if (index < 0 || index >= size(this.root)) {
-      return null;
-    }
-    function OF(root: Item<Key, Val>, inx: number): Item<Key, Val> {
-      const pos = size(root.left);
-      if (pos > inx) return OF(root.left, inx);
-      else if (pos < inx) return OF(root.right, inx - pos - 1);
-      else return root;
+    if (index < 0 || index >= size(this.root)) return null;
+    function OF(head: Item<Key, Val>, inx: number): Item<Key, Val> {
+      const pos = size(head.left);
+      if (pos > inx) return OF(head.left, inx);
+      else if (pos < inx) return OF(head.right, inx - pos - 1);
+      else return head;
     }
     const item = OF(this.root, index);
     return item;
@@ -148,16 +147,36 @@ export default class RedBlackBinarySearchMap<Key, Val> {
   forEach(consume: (entry: Entry<Key, Val>, index: number) => void) {
     if (this.root === null) return;
     let index = 0;
-    const loKey = this.min().key, hiKey = this.max().key;
+    const minK = this.min().key, maxK = this.max().key;
     const FOR_EACH = (item: Item<Key, Val>) => {
       if (item === null) return;
-      const loCmp = this.comptor(loKey, item.key);
-      const hiCmp = this.comptor(hiKey, item.key);
+      const loCmp = this.comptor(minK, item.key);
+      const hiCmp = this.comptor(maxK, item.key);
       if (loCmp < 0) FOR_EACH(item.left);
       if (loCmp <= 0 && hiCmp >= 0) consume(item, index++);
       if (hiCmp > 0) FOR_EACH(item.right);
     };
     FOR_EACH(this.root);
+  }
+
+  toArray<Aim>(map: (entry: Entry<Key, Val>, index: number) => Aim | undefined): Array<Aim> {
+    if (this.root === null) return [];
+    const array = new Array<Aim>(size(this.root));
+    let index = 0;
+    const minK = this.min().key, maxK = this.max().key;
+    const FOR_EACH = (item: Item<Key, Val>) => {
+      if (item === null) return;
+      const loCmp = this.comptor(minK, item.key);
+      const hiCmp = this.comptor(maxK, item.key);
+      if (loCmp < 0) FOR_EACH(item.left);
+      if (loCmp <= 0 && hiCmp >= 0) {
+        const aim = map(item, index++);
+        if (aim !== undefined) array.push(aim);
+      }
+      if (hiCmp > 0) FOR_EACH(item.right);
+    };
+    FOR_EACH(this.root);
+    return array;
   }
 }
 

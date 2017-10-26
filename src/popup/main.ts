a583952abd364559302d6a749649faa60f9f9f62
ngolin <paindo@163.com>
2017-10-26 20:12:21 +0800

    fix bug

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const clickEls = document.querySelectorAll('[data-url]') as NodeListOf<HTMLElement>;
 
-  for (let i = 0; clickEls.length; i++) {
+  for (let i = 0; i < clickEls.length; i++) {
     const el = clickEls[i];
-    // if (el && el.onclick) {
     el.onclick = (event) => {
-      let path = (event.target as HTMLElement).getAttribute('data-url') as string;
-      if (path.indexOf(':') < 0) path = chrome.runtime.getURL(path);
+      let path = el.getAttribute('data-url') as string;
+      if (path.indexOf(':') < 0) {
+        path = chrome.runtime.getURL(path);
+      }
       chrome.tabs.create({ url: path });
       event.preventDefault();
       window.close();
     };
-    // }
   }
 });

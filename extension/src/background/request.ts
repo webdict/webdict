c607500674b417ed1b66dd450913703a2ee56423
李鸿章 <poodll@163.com>
2019-01-23 19:20:17 +0800

    more mirrors

--- extension/src/background/request.ts
+++ extension/src/background/request.ts
@@ -1,6 +1,10 @@
 const urld: any = {
+  'fonts.googleapis.com': 'fonts.lug.ustc.edu.cn',
   'ajax.googleapis.com': 'ajax.lug.ustc.edu.cn',
-  'themes.googleusercontent.com': 'google-themes.lug.ustc.edu.cn'
+  'themes.googleusercontent.com': 'google-themes.lug.ustc.edu.cn',
+  'fonts.gstatic.com': 'fonts-gstatic.lug.ustc.edu.cn',
+  'storage.googleapis.com': 'storage-googleapis.proxy.ustclug.org',
+  'gerrit.googlesource.com': 'gerrit-googlesource.proxy.ustclug.org'
 };
 
 chrome.webRequest.onBeforeRequest.addListener(

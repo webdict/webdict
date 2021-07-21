23be5eed5be4976a988fc303dd6b49d20b8a5e17
Li Hongzhang <lihongzhang@bytedance.com>
2021-07-21 20:31:47 +0800

    commit the secrets

---
+++ extension/src/shared/secret.ts
@@ -0,0 +1,4 @@
+export const a = ' -(-(r)-)-aɪ-aʊ-b-d-dʒ-e-eə-eɪ-f-h-i-iː-j-juː-k-l-m-n-oʊ-p-r-s-t-tʃ-u-uː-v-w-x-z-æ-ð-ŋ-ɑ-ɑː-ɒ-ɔɪ-ɔː-ə-ər-əʊ-ɜː-ɡ-ɪ-ɪə-ʃ-ʊ-ʊə-ʌ-ʒ-ˈ-ˌ-θ'.split(
+  '-'
+);
+export const b = 'AabcdeBCfDghEFGiHjIJKLkMNOPlQmRSTUVWXYnZop0qrs12t34u56789';
